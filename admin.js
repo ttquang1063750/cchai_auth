a3889b3c7b9e96918f39a1fe8ab6396d33ae738c
quangtang <ttquang1063750@gmail.com>
2017-05-30 10:23:58 +0700

    init

---
+++ admin.js
@@ -0,0 +1,27 @@
+var express 		= require('express'),
+    auth 			= require('http-auth'),
+    compression   	= require('compression'),
+    path          	= require('path');;
+
+// Configure basic auth
+var basic = auth.basic({
+    realm: 'SUPER SECRET STUFF'
+}, function(username, password, callback) {
+    callback(username == 'cchai' && password == 'c-DCkrDKU=B4n-_Q');
+});
+
+// Set up express app
+var app = express();
+
+// Create middleware that can be used to protect routes with basic auth
+app.use(auth.connect(basic));
+app.use(compression());
+
+
+app.use(express.static(path.join(__dirname, '../clisss_angularjs/public/admin')));
+app.use(function(req, res, next) {
+  return res.sendFile(__dirname + '../clisss_angularjs/public/admin/index.html');
+});
+
+app.listen(8081);
+
