82dcd0fea8e01f55d6a02f494b0b995cd16d8988
Tang Thanh Quang <ttquang1063750@gmail.com>
2017-05-30 10:33:12 +0700

    fixed join path wrong address

--- admin.js
+++ admin.js
@@ -1,13 +1,13 @@
 var express 		= require('express'),
     auth 			= require('http-auth'),
     compression   	= require('compression'),
-    path          	= require('path');;
+    path          	= require('path');
 
 // Configure basic auth
 var basic = auth.basic({
     realm: 'SUPER SECRET STUFF'
 }, function(username, password, callback) {
-    callback(username == 'cchai' && password == 'c-DCkrDKU=B4n-_Q');
+    callback(username === 'cchai' && password === 'c-DCkrDKU=B4n-_Q');
 });
 
 // Set up express app
@@ -20,7 +20,7 @@ app.use(compression());
 
 app.use(express.static(path.join(__dirname, '../clisss_angularjs/public/admin')));
 app.use(function(req, res, next) {
-  return res.sendFile(__dirname + '../clisss_angularjs/public/admin/index.html');
+  return res.sendFile(path.join(__dirname, '../clisss_angularjs/public/admin/index.html'));
 });
 
 app.listen(8081);
--- client.js
+++ client.js
@@ -1,13 +1,13 @@
-var express 		= require('express'),
-    auth 			= require('http-auth'),
-    compression   	= require('compression'),
-    path          	= require('path');;
+var express = require('express'),
+    auth = require('http-auth'),
+    compression = require('compression'),
+    path = require('path');
 
 // Configure basic auth
 var basic = auth.basic({
-    realm: 'SUPER SECRET STUFF'
-}, function(username, password, callback) {
-    callback(username == 'cchai' && password == 'c-DCkrDKU=B4n-_Q');
+  realm: 'SUPER SECRET STUFF'
+}, function (username, password, callback) {
+  callback(username === 'cchai' && password === 'c-DCkrDKU=B4n-_Q');
 });
 
 // Set up express app
@@ -18,9 +18,9 @@ app.use(auth.connect(basic));
 app.use(compression());
 
 
-app.use(express.static(path.join(__dirname, '../clisss_angularjs/public/admin')));
-app.use(function(req, res, next) {
-  return res.sendFile(__dirname + '../clisss_angularjs/public/client/index.html');
+app.use(express.static(path.join(__dirname, '../clisss_angularjs/public/client')));
+app.use(function (req, res, next) {
+  return res.sendFile(path.join(__dirname, '../clisss_angularjs/public/client/index.html'));
 });
 
 app.listen(8080);
